c8da5c4085cbb777ea06b0196244d6d02db78005
Gmailskie <1982tonnata@gmail.com>
2025-04-18 12:52:23 +0700

    fast commit ($username)

--- fast_commit.bat
+++ fast_commit.bat
@@ -8,9 +8,10 @@ set "script_dir=%~dp0"
 cd /d "%script_dir%"
 
 :: Здесь вы можете добавить команды, которые хотите выполнить в этой директории
+username=$(git config user.name)
 for /f "delims=" %%b in ('git rev-parse --abbrev-ref HEAD') do set "current_branch=%%b"
 git add .
-git commit -m "fast commit (%current_branch%)"
+git commit -m "fast commit ($username)"
 git push origin %current_branch%
 
 endlocal
