0f21c02e0c0434c364eccf457e9c04aec0622484
gmailskie <1982tonnata@gmail.com>
2025-04-19 00:18:47 +0700

    fast commit (gmailskie)

---
+++ FastGit.bat
@@ -0,0 +1,61 @@
+@echo off
+chcp 65001 > nul
+cls
+
+:menu
+echo.
+echo  =========== Fast Git ===========
+echo.
+echo  1. Сделать быстрый коммит       
+echo  2. Сделать обычный коммит       
+echo  3. Переключиться на другую ветку
+echo  4. Выход                        
+echo.
+set /p choice="Выберите действие (1-4): "
+
+if "%choice%"=="1" goto fast_commit
+if "%choice%"=="2" goto commit
+if "%choice%"=="3" goto checkout
+if "%choice%"=="4" goto quit
+
+echo Ты написал что-то кроме предложенных вариантов
+pause
+goto menu
+
+:fast_commit
+setlocal
+set "script_dir=%~dp0"
+for /f "delims=" %%i in ('git config user.name') do set "username=%%i"
+cd /d "%script_dir%"
+username=$(git config user.name)
+for /f "delims=" %%b in ('git rev-parse --abbrev-ref HEAD') do set "current_branch=%%b"
+git add .
+git commit -m "fast commit (%username%)"
+git push origin %current_branch%
+endlocal
+goto menu
+
+:commit
+setlocal
+set "script_dir=%~dp0"
+cd /d "%script_dir%"
+set /p user_input="Комментарий: "
+for /f "delims=" %%b in ('git rev-parse --abbrev-ref HEAD') do set "current_branch=%%b"
+git add .
+git commit -m "%user_input% (%username%)"
+git push origin %current_branch%
+endlocal
+goto menu
+
+:checkout
+setlocal
+set "script_dir=%~dp0"
+cd /d "%script_dir%"
+git branch
+set /p branch_name="Введи название ветки: "
+git checkout %branch_name%
+endlocal
+goto menu
+
+:quit
+exit
--- commit.bat
+++ commit.bat
@@ -9,10 +9,11 @@ set "script_dir=%~dp0"
 cd /d "%script_dir%"
 
 :: Здесь вы можете добавить команды, которые хотите выполнить в этой директории
+username=$(git config user.name)
 set /p user_input="Комментарий: "
 for /f "delims=" %%b in ('git rev-parse --abbrev-ref HEAD') do set "current_branch=%%b"
 git add .
-git commit -m "%user_input% (%current_branch%)"
+git commit -m "%user_input% (%username%)"
 git push origin %current_branch%
 
 endlocal
