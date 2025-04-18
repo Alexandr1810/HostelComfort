104cec833faed60d36e8e71747966a9382eaf44f
Gmailskie <1982tonnata@gmail.com>
2025-04-18 12:54:56 +0700

    fast commit (Gmailskie)

--- fast_commit.bat
+++ fast_commit.bat
@@ -4,6 +4,9 @@ setlocal
 :: Получаем путь к директории, где находится этот .bat файл
 set "script_dir=%~dp0"
 
+:: Получаем имя пользователя Git
+for /f "delims=" %%i in ('git config user.name') do set "username=%%i"
+
 :: Переходим в директорию скрипта
 cd /d "%script_dir%"
 
@@ -11,7 +14,7 @@ cd /d "%script_dir%"
 username=$(git config user.name)
 for /f "delims=" %%b in ('git rev-parse --abbrev-ref HEAD') do set "current_branch=%%b"
 git add .
-git commit -m "fast commit ($username)"
+git commit -m "fast commit (%username%)"
 git push origin %current_branch%
 
 endlocal
