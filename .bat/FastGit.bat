80d71831f9b5de7bcfd15132c2da28172844bf0b
Gmailskie <1982tonnata@gmail.com>
2025-04-19 21:28:39 +0700

    fast commit (Gmailskie)

--- .bat/FastGit.bat
+++ .bat/FastGit.bat
@@ -4,22 +4,26 @@ cls
 
 :menu
 echo.
-echo ============= Fast Git =============
-echo.=                                  =
-echo = 1. Сделать быстрый коммит        =
-echo = 2. Сделать обычный коммит        =
-echo = 3. Переключиться на другую ветку =
-echo = 4. Создать новую ветку           =
-echo = 5. Выход                         =
-echo ====================================
+echo ============= Fast Git ==============
+echo.=                                   =
+echo = 1. Сделать быстрый коммит         =
+echo = 2. Сделать обычный коммит         =
+echo = 3. Переключиться на другую ветку  =
+echo = 4. Создать новую ветку            =
+echo = 5. Удалить определённую ветку     =
+echo = 6. Выполнить слияние(merge) веток =
+echo = 7. Выход                          =
+echo =====================================
 echo.
-set /p choice="Выберите действие (1-5): "
+set /p choice="Выберите действие (1-7): "
 
 if "%choice%"=="1" goto fast_commit
 if "%choice%"=="2" goto commit
 if "%choice%"=="3" goto checkout
 if "%choice%"=="4" goto create_new_branch
-if "%choice%"=="5" goto quit
+if "%choice%"=="5" goto delete_branch
+if "%choice%"=="6" goto merge
+if "%choice%"=="7" goto quit
 
 echo Ты написал что-то кроме предложенных вариантов
 pause
@@ -64,17 +68,37 @@ goto menu
 	setlocal
 	set "script_dir=%~dp0"
 	cd /d "%script_dir%"
-
+	set /p branch_name1="Введи название ветки которую возьмёте за основу новой: "
 	set /p branch_name="Введи название создаваемой ветки: "
 	if "%branch_name%"=="" (
     echo Ошибка: имя ветки не может быть пустым!
     goto create_new_branch
 	)
 	
+	git checkout %branch_name1%
 	git branch %branch_name%
 	git push origin %branch_name%
 	echo Ветка "%branch_name%" создана
 	endlocal
+	goto menu
+
+:merge
+	setlocal
+	set "script_dir=%~dp0"
+	cd /d "%script_dir%"
+	set /p branch_name1="Введи название ветки в которую нужно внести изменения: "
+	set /p branch_name2="Введи название ветки из которой нужно взять изменения: "
+	git checkout %branch_name1%
+	git pull origin %branch_name1%
+	git merge %branch_name2%
+	set /p user_input="Комментарий: "
+	if "%user_input%"=="" (
+		git commit -m "fast merge"
+		git push origin %branch_name1%
+		goto menu
+	)
+	git commit -m "%user_input%"
+	git push origin %branch_name1%
 	goto menu
 
 :quit
