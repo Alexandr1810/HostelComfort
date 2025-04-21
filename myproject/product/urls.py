3d4da6716e867d27ff9076eb6310db7154b2707e
Alexandr <152762537+Alexandr1810@users.noreply.github.com>
2025-04-21 12:54:03 +0700

    сделал шапку

--- myproject/product/urls.py
+++ myproject/product/urls.py
@@ -2,24 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-<<<<<<< HEAD
-    path('', views.hotel, name = 'hotel'),
-    path('hotel_info/<int:id>/', views.hotel_info, name='hotel_info'),
-    path('booking/<int:id>/', views.booking, name = 'booking'),
-    path('booking_info/<int:id>/', views.booking_info, name = 'booking_info'),
-    path('login/', views.login, name = 'login'),
-    path('register/', views.register, name = 'register'),
-    path('profile/', views.profile, name = 'profile'),
-    
-    #frontend
-    path('hotel/', views.hotel_1, name = 'hotel_1'),
-    path('hotel_info_frontend/', views.hotel_info_1, name='hotel_info_1'),
-    path('booking_frontend/', views.booking_1, name = 'booking_1'),
-    path('booking_info_frontend/', views.booking_info_1, name = 'booking_info_1'),
-    path('login_frontend/', views.login_1, name = 'login_1'),
-    path('register_frontend/', views.register_1, name = 'register_1'),
-    path('profile_frontend/', views.profile_1, name = 'profile_1'),
-=======
     path('', views.hotel, name='hotel'),
     path('hotel/<int:id>/', views.hotel_detail, name='hotel_detail'),
     path('booking/<int:id>/', views.booking, name='booking'),
@@ -28,5 +10,14 @@ urlpatterns = [
     path('logout/', views.user_logout, name='logout'),
     path('register/', views.register, name='register'),
     path('profile/', views.user_profile, name='user_profile'),
->>>>>>> ec21fd3f5b4b67e386a4fb090478e03e638ca386
+    
+    #frontend
+    path('hotel/', views.hotel_1, name = 'hotel_1'),
+    path('hotel_frontend/', views.hotel_detail, name='hotel_detail_1'),
+    path('booking_frontend/', views.booking_1, name = 'booking_1'),
+    path('booking_info_frontend/', views.booking_info_1, name = 'booking_info_1'),
+    path('login_frontend/', views.user_login_1, name = 'login_1'),
+    path('logout_frontend/', views.user_logout_1, name='user_login_1'),
+    path('register_frontend/', views.register_1, name = 'register_1'),
+    path('profile_frontend/', views.user_profile_1, name = 'user_profile_1'),
 ]
--- myproject/product/views.py
+++ myproject/product/views.py
@@ -19,44 +19,6 @@ def hotel_detail(request, id):
     return render(request, 'hotel/hotel_info.html', context)
 
 def booking(request, id):
-<<<<<<< HEAD
-    hotel = get_object_or_404(Hotel, id=id)  # Получаем отель по ID
-    return render(request, 'booking.html', {'hotel': hotel})
-
-def booking_info(request, id):
-    hotel = get_object_or_404(Hotel, id=id)  # Получаем отель по ID
-    return render(request, 'booking_info.html', {'hotel': hotel})
-
-
-def hotel_1(request):
-    return render(request, 'frontend/hotel/index.html')
-
-def hotel_info_1(request):
-    return render(request, 'frontend/hotel/hotel_info.html')
-  
-def booking_1(request):
-    return render(request, 'frontend/hotel/booking.html')
-
-def booking_info_1(request):
-    return render(request, 'frontend/hotel/booking_info.html')
-
-def login_1(request):
-    return render(request, 'frontend/registration/login.html')
-  
-def register_1(request):
-    return render(request, 'frontend/registration/register.html')
-
-def profile_1(request):
-    return render (request, 'frontend/profile/user.html')
-
-def hotel_1(request):
-    hotel = Hotel.objects.all()
-    return render(request, 'frontend/hotel/index.html', {'hotel': hotel})
-
-def hotel_info_1(request):
-    hotel_info_1 = Room.objects.all()
-    return render(request, 'frontend/hotel/hotel_info.html', {'hotel_info_1': hotel_info_1})
-=======
     hotel = get_object_or_404(Hotel, id=id)
     return render(request, 'hotel/booking.html', {'hotel': hotel})
 
@@ -120,4 +82,74 @@ def user_profile(request):
         'client': client,
         'reservations': reservations
     })
->>>>>>> ec21fd3f5b4b67e386a4fb090478e03e638ca386
+
+#frontend
+
+def hotel_1(request):
+    return render(request, 'frontend/hotel/index.html')
+
+def hotel_detail_1(request):
+    return render(request, 'frontend/hotel/hotel_info.html')
+  
+def booking_1(request):
+    return render(request, 'frontend/hotel/booking.html')
+
+def booking_info_1(request):
+    return render(request, 'frontend/hotel/booking_info.html')
+
+def register_1(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            Clients.objects.create(
+                user=user,
+                phio=form.cleaned_data['phio'],
+                phone=form.cleaned_data['phone'],
+                email=form.cleaned_data['email'],
+                passport_seria=form.cleaned_data['passport_seria'],
+                passport_num=form.cleaned_data['passport_num']
+            )
+            login(request, user)
+            return redirect('hotel')  # Перенаправляем на главную страницу
+        else:
+            # Добавляем сообщения об ошибках
+            for field, errors in form.errors.items():
+                for error in errors:
+                    messages.error(request, f"{field}: {error}")
+    else:
+        form = RegisterForm()
+    
+    return render(request, 'registration/register.html', {'form': form})
+
+def user_login_1(request):
+    if request.method == 'POST':
+        form = LoginForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('user_profile')
+        messages.error(request, 'Неверный телефон/email или пароль')
+    else:
+        form = LoginForm()
+    return render(request, 'registration/login.html', {'form': form})
+
+def user_logout_1(request):
+    logout(request)
+    return redirect('login')
+
+@login_required
+def user_profile_1(request):
+    try:
+        client = request.user.clients  # Пытаемся получить связанного клиента
+    except ObjectDoesNotExist:
+        # Если клиент не существует, перенаправляем на заполнение профиля
+        return redirect('complete_profile')
+    reservations = Reservations.objects.filter(client_id=client)
+    return render(request, 'profile/user.html', {
+        'client': client,
+        'reservations': reservations
+    })
