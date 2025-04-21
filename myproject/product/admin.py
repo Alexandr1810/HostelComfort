ec21fd3f5b4b67e386a4fb090478e03e638ca386
B1ack29 <dead7190@mail.ru>
2025-04-21 10:10:36 +0700

    домашка сереги

--- myproject/product/admin.py
+++ myproject/product/admin.py
@@ -1,9 +1,21 @@
 from django.contrib import admin
 from .models import Hotel, Room, Clients, Reservations, Reviews_and_ratings
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 
+class ClientsInline(admin.StackedInline):
+    model = Clients
+    can_delete = False
+    verbose_name_plural = 'Дополнительная информация'
+
+class CustomUserAdmin(UserAdmin):
+    inlines = (ClientsInline,)
+
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
+admin.site.register(Clients)
 admin.site.register(Hotel)
 admin.site.register(Room)
-admin.site.register(Clients)
 admin.site.register(Reservations)
 admin.site.register(Reviews_and_ratings)
 
--- myproject/product/urls.py
+++ myproject/product/urls.py
@@ -1,28 +1,13 @@
-"""
-URL configuration for example project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.hotel, name = 'hotel'),
-    path('hotel_info/<int:id>/', views.hotel_info, name='hotel_info'),
-    path('booking/<int:id>/', views.booking, name = 'booking'),
-    path('booking_info/<int:id>/', views.booking_info, name = 'booking_info'),
-    path('login/', views.login, name = 'login'),
-    path('register/', views.register, name = 'register'),
-    path('profile/', views.profile, name = 'profile'),
+    path('', views.hotel, name='hotel'),
+    path('hotel/<int:id>/', views.hotel_detail, name='hotel_detail'),
+    path('booking/<int:id>/', views.booking, name='booking'),
+    path('booking_info/<int:id>/', views.booking_info, name='booking_info'),
+    path('login/', views.user_login, name='login'),
+    path('logout/', views.user_logout, name='logout'),
+    path('register/', views.register, name='register'),
+    path('profile/', views.user_profile, name='user_profile'),
 ]
