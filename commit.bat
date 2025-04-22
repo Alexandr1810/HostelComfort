6ad9fd7d45f1b29381e6e6bff986b4ff124f2b96
Gmailskie <1982tonnata@gmail.com>
2025-04-22 09:42:13 +0700

    fast commit (ilya)

--- commit.bat
+++ commit.bat
@@ -9,7 +9,10 @@ set "script_dir=%~dp0"
 cd /d "%script_dir%"
 
 :: Здесь вы можете добавить команды, которые хотите выполнить в этой директории
+set /p user_input="Комментарий: "
 for /f "delims=" %%b in ('git rev-parse --abbrev-ref HEAD') do set "current_branch=%%b"
+git add .
+git commit -m "%user_input% (%current_branch%)"
 git push origin %current_branch%
 
 endlocal
